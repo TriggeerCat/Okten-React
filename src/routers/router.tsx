0e1a0fec47d7119b930f6717f227562acda4c2fe
TriggeerCat <zspwdmnplay@gmail.com>
2025-04-19 19:40:10 +0300

    feat (3.1): added assignment code

---
+++ src/routers/router.tsx
@@ -0,0 +1,11 @@
+﻿import {createBrowserRouter} from "react-router-dom";
+import MainLayout from "../layouts/MainLayout.tsx";
+
+export const router = createBrowserRouter([
+    {path: '', element: <MainLayout/>, children: [
+            {path: 'users', element: <div className='text-xl'>Users</div>},
+            {path: 'posts', element: <div className='text-xl'>Posts</div>},
+            {path: 'comments', element: <div className='text-xl'>Comments</div>},
+            {path: 'products', element: <div className='text-xl'>Products</div>}
+        ]}
+])
